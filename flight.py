97e65934f9e1d2ee71d5727ba481394da5c36678
om3rr <Omertorrents@gmail.com>
2019-07-27 01:49:44 +0300

    python :X

--- flight.py
+++ flight.py
@@ -1,5 +1,6 @@
 from alchemy import Base
-from sqlalchemy import Column, Integer, String, func
+import datetime
+from sqlalchemy import Column, Integer, String, func, DateTime
 from sqlalchemy.orm import Query
 class Flight(Base):
     __tablename__ = 'flights'
@@ -8,8 +9,8 @@ class Flight(Base):
     f_country = Column(String(250), nullable=False)
     f_city = Column(String(250), nullable=False)
     f_price = Column(Integer, nullable=False)
-    f_from = Column(String, nullable=False)
-    f_to = Column(String, nullable=False)
+    f_from = Column(DateTime, nullable=False)
+    f_to = Column(DateTime, nullable=False)
 
     @staticmethod
     def factory(flight):
@@ -18,13 +19,33 @@ class Flight(Base):
         args["f_country"] = flight.get("DealCountryName")
         args["f_city"] = flight.get("DealDestinationName")
         args["f_price"] = flight.get("PriceInShekel")
-        args["f_from"] = flight.get("OutboundFlights")[0].get("DepartureATA")
-        args["f_to"] = flight.get("InboundFlights")[-1].get("DepartureATA")
+        args["f_from"] = Flight.format_time(flight.get("OutboundFlights")[0].get("DepartureATA"))
+        args["f_to"] = Flight.format_time(flight.get("InboundFlights")[-1].get("DepartureATA"))
         return Flight(**args)
+
+    @staticmethod
+    def format_time(str_time):
+        return datetime.datetime.strptime(str_time, '%d/%m/%Y %H:%M')
 
 
     def already_exists(self, session) -> bool:
         return Query(func.count(Flight.id), session=session).filter(Flight.f_id == self.f_id).filter(Flight.f_price <= self.f_price).first()[0] == 1
+
+    @staticmethod
+    def find_by_country(session, country):
+        return Flight.basic_query(session, lambda x: x.filter(Flight.f_country == country))
+
+    @staticmethod
+    def find_by_city(session, city):
+        return Flight.basic_query(session, lambda x: x.filter(Flight.f_city == city))
+
+    @staticmethod
+    def basic_query(session, block):
+        query = Query([Flight], session)
+        query = block(query)
+        return query.filter(Flight.f_from > datetime.datetime.now() + datetime.timedelta(days=1)) \
+            .order_by(Flight.f_price) \
+            .all()
 
     @property
     def href(self):
